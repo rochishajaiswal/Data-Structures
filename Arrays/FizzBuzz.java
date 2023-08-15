6a12989ce675155a116a6acdd777370bc672013c
Jaiswal <JAISWRO2@novartis.net>
2023-08-15 21:21:26 +0530

    adding new arrays folder

--- Arrays/FizzBuzz.java
+++ Arrays/FizzBuzz.java
@@ -1,5 +1,4 @@
-/*
-Given an integer n, return a string array answer (1-indexed) where:
+/*Given an integer n, return a string array answer (1-indexed) where:
 
 answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
 answer[i] == "Fizz" if i is divisible by 3.
@@ -10,22 +9,23 @@ answer[i] == i (as a string) if none of the above conditions are true.
 Example 1:
 
 Input: n = 3
-Output: ["1","2","Fizz"]
-*/
+Output: ["1","2","Fizz"]*/
 
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String>ans=new ArrayList<String>();
-        for(int i=1;i<=n;i++){
-            if(i%15==0)
-            ans.add("FizzBuzz");
-            else if(i%5==0)
-            ans.add("Buzz");
-            else if(i%3==0)
-            ans.add("Fizz");
+        List<String> ans = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0)
+                ans.add("FizzBuzz");
+            else if (i % 5 == 0)
+                ans.add("Buzz");
+            else if (i % 3 == 0)
+                ans.add("Fizz");
             else
-            ans.add(String.valueOf(i));
+                ans.add(String.valueOf(i));
         }
         return ans;
     }
