773612b7df4eea35d5bc9a555f7f3456e56995c2
Jaiswal <JAISWRO2@novartis.net>
2023-08-16 22:59:51 +0530

    adding new problem

--- Strings/SearchAll.java
+++ Strings/SearchAll.java
@@ -1,1 +1,34 @@
-abc
+
+/*Implement a function that returns a list of all occurrences of a given  substring inside a big string.
+Return empty vector if smaller string is not present inside bigger string.
+
+Sample Input
+
+string bigString = "I liked the movie, acting in movie was great!";
+string smallString = "movie"
+
+
+Sample Output
+12, 29
+*/
+import java.util.*;
+
+public class SearchAll {
+
+    public static void main(String[] args) {
+        String big = "I liked the movie, acting in movie was great!";
+        String small = "movie";
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int index = 0;
+        while (index != -1) {
+            index = big.indexOf(small, index);
+            if (index == -1)
+                break;
+            else
+                ans.add(index);
+            index++;
+        }
+        for (int a : ans)
+            System.out.println(a);
+    }
+}
